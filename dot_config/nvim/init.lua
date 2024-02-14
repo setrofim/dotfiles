7aba87d112147b964228e79a248c6fc3e023aa05
Sergei Trofimov <sergei.trofimov@arm.com>
2024-02-14 11:17:19 +0000

    nvim: autocompletion and gruvbox tweaks

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -33,11 +33,11 @@ vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist)
 vim.keymap.set('n', '<space>e', vim.diagnostic.open_float)
 
 function MapF1()
-	if vim.bo.buftype == 'help' then
-		vim.cmd('quit')
-	else
-		vim.cmd('help')
-	end
+    if vim.bo.buftype == 'help' then
+        vim.cmd('quit')
+    else
+        vim.cmd('help')
+    end
 end
 vim.keymap.set('i', '<F1>', '<Esc>') -- I keep hitting this by mistake
 vim.keymap.set('n', '<F1>', ':lua MapF1()<CR>')
@@ -56,27 +56,27 @@ call matchadd('Trail', '\s\+$', 100)
 ]])
 
 local ft_maps = {
-	{{'*dts', '*dtsi'}, 'set filetype=dts'},
-	{{'*docker'}, 'set filetype=dockerfile'},
-	{{'run.log'}, 'set filetype=walog'},
-	{{'*.weechat'}, 'set filetype=irc'},
-	{{'*.xaml'}, 'set filetype=xml'},
-	{{'*.html'}, 'set filetype=htmljinja'},
-	{{'*.md'}, 'set filetype=markdown'},
-	{{'*.objdump'}, 'set filetype=objdump'},
-	{{'*.sqlite*'}, 'set filetype=sqlite'},
-	{{'*PULLREQ_EDITMSG'}, 'set spell textwidth=80'},
-	{{'*COMMIT_EDITMSG'}, 'set spell textwidth=80'},
-	{{'*.doku'}, 'set filetype=dokuwiki'},
-	{{'*.tmpl'}, 'set filetype=jinja'},
-	{{'*.rst'}, 'set filetype=rrst textwidth=80'},
+    {{'*dts', '*dtsi'}, 'set filetype=dts'},
+    {{'*docker'}, 'set filetype=dockerfile'},
+    {{'run.log'}, 'set filetype=walog'},
+    {{'*.weechat'}, 'set filetype=irc'},
+    {{'*.xaml'}, 'set filetype=xml'},
+    {{'*.html'}, 'set filetype=htmljinja'},
+    {{'*.md'}, 'set filetype=markdown'},
+    {{'*.objdump'}, 'set filetype=objdump'},
+    {{'*.sqlite*'}, 'set filetype=sqlite'},
+    {{'*PULLREQ_EDITMSG'}, 'set spell textwidth=80'},
+    {{'*COMMIT_EDITMSG'}, 'set spell textwidth=80'},
+    {{'*.doku'}, 'set filetype=dokuwiki'},
+    {{'*.tmpl'}, 'set filetype=jinja'},
+    {{'*.rst'}, 'set filetype=rrst textwidth=80'},
 }
 
 for _, ftm in ipairs(ft_maps) do
-	vim.api.nvim_create_autocmd(
-		{'BufRead', 'BufNewFile'},
-		{pattern = ftm[1], command = ftm[2]}
-	)
+    vim.api.nvim_create_autocmd(
+        {'BufRead', 'BufNewFile'},
+        {pattern = ftm[1], command = ftm[2]}
+    )
 end
 
 
@@ -92,82 +92,160 @@ vim.g['airline_powerline_fonts']= 1
 
 require('plugins')
 
+-- autocompletion / cmp (needs to happend before lsp setup -- see capabilities below
+local cmp = require('cmp')
+
+cmp.setup({
+    snippet = {
+        -- REQUIRED - you must specify a snippet engine
+        expand = function(args)
+            vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+            -- require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+            -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
+            -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
+        end,
+    },
+    window = {
+        -- completion = cmp.config.window.bordered(),
+        -- documentation = cmp.config.window.bordered(),
+    },
+    mapping = cmp.mapping.preset.insert({
+        ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+        ['<C-f>'] = cmp.mapping.scroll_docs(4),
+        ['<C-Space>'] = cmp.mapping.complete(),
+        ['<C-e>'] = cmp.mapping.abort(),
+        ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+    }),
+    sources = cmp.config.sources({
+        { name = 'nvim_lsp' },
+        { name = 'vsnip' }, -- For vsnip users.
+        -- { name = 'luasnip' }, -- For luasnip users.
+        -- { name = 'ultisnips' }, -- For ultisnips users.
+        -- { name = 'snippy' }, -- For snippy users.
+    }, {
+        { name = 'buffer' },
+    })
+})
+
+-- Set configuration for specific filetype.
+cmp.setup.filetype('gitcommit', {
+    sources = cmp.config.sources({
+        { name = 'git' }, -- You can specify the `git` source if [you were installed it](https://github.com/petertriho/cmp-git).
+    }, {
+        { name = 'buffer' },
+    })
+})
+
+-- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline({ '/', '?' }, {
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = {
+        { name = 'buffer' }
+    }
+})
+
+-- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline(':', {
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = cmp.config.sources({
+        { name = 'path' }
+    }, {
+        { name = 'cmdline' }
+    })
+})
+
+-- Set up lspconfig.
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
 -- lauguage servers
-require('lspconfig').pyright.setup{}
-require('lspconfig').gopls.setup{}
-require('lspconfig').rust_analyzer.setup{}
-require('lspconfig').ccls.setup{}
+require('lspconfig').pyright.setup{ capabilities = capabilities }
+require('lspconfig').gopls.setup{ capabilities = capabilities }
+require('lspconfig').rust_analyzer.setup{ capabilities = capabilities }
+require('lspconfig').ccls.setup{ capabilities = capabilities }
 require('lspconfig').bashls.setup{
-	filetypes = { "sh", "bash"},
+    capabilities = capabilities,
+    filetypes = { "sh", "bash"},
 }
 require('lspconfig').lua_ls.setup{
-  on_init = function(client)
-    local path = client.workspace_folders[1].name
-    if not vim.loop.fs_stat(path..'/.luarc.json') and not vim.loop.fs_stat(path..'/.luarc.jsonc') then
-      client.config.settings = vim.tbl_deep_extend('force', client.config.settings, {
-        Lua = {
-          runtime = {
-            -- Tell the language server which version of Lua you're using
-            -- (most likely LuaJIT in the case of Neovim)
-            version = 'LuaJIT'
-          },
-          -- Make the server aware of Neovim runtime files
-          workspace = {
-            checkThirdParty = false,
-            library = {
-              vim.env.VIMRUNTIME
-              -- "${3rd}/luv/library"
-              -- "${3rd}/busted/library",
-            }
-            -- or pull in all of 'runtimepath'. NOTE: this is a lot slower
-            -- library = vim.api.nvim_get_runtime_file("", true)
-          }
-        }
-      })
+    capabilities = capabilities,
+    on_init = function(client)
+        local path = client.workspace_folders[1].name
+        if not vim.loop.fs_stat(path..'/.luarc.json') and not vim.loop.fs_stat(path..'/.luarc.jsonc') then
+            client.config.settings = vim.tbl_deep_extend('force', client.config.settings, {
+                Lua = {
+                    runtime = {
+                        -- Tell the language server which version of Lua you're using
+                        -- (most likely LuaJIT in the case of Neovim)
+                        version = 'LuaJIT'
+                    },
+                    -- Make the server aware of Neovim runtime files
+                    workspace = {
+                        checkThirdParty = false,
+                        library = {
+                            vim.env.VIMRUNTIME
+                            -- "${3rd}/luv/library"
+                            -- "${3rd}/busted/library",
+                        }
+                        -- or pull in all of 'runtimepath'. NOTE: this is a lot slower
+                        -- library = vim.api.nvim_get_runtime_file("", true)
+                    }
+                }
+            })
 
-      client.notify("workspace/didChangeConfiguration", { settings = client.config.settings })
+            client.notify("workspace/didChangeConfiguration", { settings = client.config.settings })
+        end
+        return true
     end
-    return true
-  end
 }
 
 -- Use LspAttach autocommand to only map the following keys
 -- after the language server attaches to the current buffer
 vim.api.nvim_create_autocmd('LspAttach', {
-  group = vim.api.nvim_create_augroup('UserLspConfig', {}),
-  callback = function(ev)
-    -- Enable completion triggered by <c-x><c-o>
-    vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
+    group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+    callback = function(ev)
+        -- Enable completion triggered by <c-x><c-o>
+        vim.o.signcolumn = 'yes'
+        vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
 
-    -- Buffer local mappings.
-    -- See `:help vim.lsp.*` for documentation on any of the below functions
-    local opts = { buffer = ev.buf }
-    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
-    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-    vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
-    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
-    vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
-    vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
-    vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
-    vim.keymap.set('n', '<space>wl', function()
-      print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-    end, opts)
-    vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
-    vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
-    vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
-    vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
-    vim.keymap.set('n', '<space>f', function()
-      vim.lsp.buf.format { async = true }
-    end, opts)
-  end,
+        -- Buffer local mappings.
+        -- See `:help vim.lsp.*` for documentation on any of the below functions
+        local opts = { buffer = ev.buf }
+        vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+        vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+        vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+        vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+        vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
+        vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
+        vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
+        vim.keymap.set('n', '<space>wl', function()
+            print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+        end, opts)
+        vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
+        vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
+        vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
+        vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+        vim.keymap.set('n', '<space>f', function()
+            vim.lsp.buf.format { async = true }
+        end, opts)
+    end,
 })
 
--- colorscheme
+-- colorscheme / gruvbox
 vim.opt.background = 'dark'
-vim.cmd('colorscheme gruvbox')
 vim.g.gruvbox_improved_strings = 1
 vim.g.gruvbox_improved_warnings = 1
 
+local gruvbox = require("gruvbox")
+gruvbox.setup({
+    overrides = {
+        SignColumn = { bg = gruvbox.palette.dark0 },
+        GruvboxRedSign = { fg = gruvbox.palette.red, bg = gruvbox.palette.dark0 },
+        GruvboxYellowSign = { fg = gruvbox.palette.yellow, bg = gruvbox.palette.dark0 },
+        GruvboxBlueSign = { fg = gruvbox.palette.blue, bg = gruvbox.palette.dark0 },
+        GruvboxAquaSign = { fg = gruvbox.palette.aqua, bg = gruvbox.palette.dark0 },
+    }
+})
+vim.cmd('colorscheme gruvbox')
 
 -- ranger
 vim.g.ranger_replace_netrw = 1
@@ -181,13 +259,13 @@ vim.keymap.set('n', '<leader>ev', ':BufExplorerVerticalSplit<CR>')
 
 -- aerial
 require('aerial').setup({
-  show_guides = true,
-  close_automatic_events = {'unfocus', 'switch_buffer', 'unsupported'},
-  on_attach = function(bufnr)
-    -- Jump forwards/backwards with '{' and '}'
-    vim.keymap.set('n', '{', '<cmd>AerialPrev<CR>', {buffer = bufnr})
-    vim.keymap.set('n', '}', '<cmd>AerialNext<CR>', {buffer = bufnr})
-  end
+    show_guides = true,
+    close_automatic_events = {'unfocus', 'switch_buffer', 'unsupported'},
+    on_attach = function(bufnr)
+        -- Jump forwards/backwards with '{' and '}'
+        vim.keymap.set('n', '{', '<cmd>AerialPrev<CR>', {buffer = bufnr})
+        vim.keymap.set('n', '}', '<cmd>AerialNext<CR>', {buffer = bufnr})
+    end
 })
 
 vim.keymap.set('n', '<F8>', ':AerialToggle<CR>')
@@ -196,11 +274,11 @@ vim.keymap.set('n', '<F8>', ':AerialToggle<CR>')
 MiniMap = require("mini.map")
 
 MiniMap.setup({
- symbols = {
-   encode = MiniMap.gen_encode_symbols.dot('4x2'),
-   scroll_view = '┋',
-   scroll_line = '▶',
- },
+    symbols = {
+        encode = MiniMap.gen_encode_symbols.dot('4x2'),
+        scroll_view = '┋',
+        scroll_line = '▶',
+    },
 })
 
 vim.keymap.set('n', '<Leader>mc', MiniMap.close)
@@ -217,9 +295,9 @@ vim.keymap.set('n', '<M-l>', ':SidewaysRight<CR>')
 -- vim-filtering
 vim.cmd([[
 function! FilterUserInput()
-        let obj =  FilteringNew()
-        call obj.parseQuery(input('>'), '|')
-        call obj.run()
+    let obj =  FilteringNew()
+    call obj.parseQuery(input('>'), '|')
+    call obj.run()
 endfunction
 ]])
 vim.keymap.set('n', ',F', ':call FilterUserInput()<CR>')
@@ -244,3 +322,35 @@ vim.keymap.set('n', ',fd', builtin.lsp_definitions, {})
 vim.keymap.set('n', ',fr', builtin.lsp_references, {})
 vim.keymap.set('n', ',fI', builtin.lsp_implementations, {})
 vim.keymap.set('n', ',fD', builtin.diagnostics, {})
+
+-- treesitter
+require('nvim-treesitter.configs').setup({
+    -- A directory to install the parsers into.
+    -- If this is excluded or nil parsers are installed
+    -- to either the package dir, or the "site" dir.
+    -- If a custom path is used (not nil) it must be added to the runtimepath.
+    parser_install_dir = "~/.cache/treesitter",
+
+    -- A list of parser names, or "all"
+    -- ensure_installed = { "c", "java", "kotlin", "lua", "go", "python", "rust" },
+
+    -- Install parsers synchronously (only applied to `ensure_installed`)
+    sync_install = false,
+
+    -- Automatically install missing parsers when entering buffer
+    auto_install = true,
+
+    highlight = {
+        -- `false` will disable the whole extension
+        enable = true,
+
+        -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+        -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+        -- Using this option may slow down your editor, and you may see some duplicate highlights.
+        -- Instead of true it can also be a list of languages
+        additional_vim_regex_highlighting = false,
+    },
+})
+vim.opt.runtimepath:append("~/.cache/treesitter")
+
+-- vim: set et sts=4  sw=4:
--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -3,63 +3,75 @@
 --
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
 end
 vim.opt.rtp:prepend(lazypath)
 
 require('lazy').setup({
-  'wbthomason/packer.nvim', -- plugin manager (can manage itself)
+    'wbthomason/packer.nvim', -- plugin manager (can manage itself)
 
-  'mfussenegger/nvim-dap', -- java debug
-  'mfussenegger/nvim-jdtls', -- java language server
-  'nvim-lua/plenary.nvim', -- additional lua functions
-  'neovim/nvim-lspconfig', -- language server configuration
-  'rafaqz/ranger.vim',  -- use ranger as file browser
-  'ellisonleao/gruvbox.nvim', -- colorscheme
-  'jlanzarotta/bufexplorer', -- better buffer navigation
-  'echasnovski/mini.map', -- mini map (buffer text overview)
-  'tpope/vim-fugitive', -- git integration
-  'scrooloose/nerdcommenter', -- code commenting
-  'nielsadb/vim-filtering', -- filter lines in buffer
-  {
-    'nvim-telescope/telescope.nvim', tag = '0.1.5',
-    dependencies = {
-        'nvim-lua/plenary.nvim',
-        'ryanoasis/vim-devicons',
+    'mfussenegger/nvim-dap', -- java debug
+    'mfussenegger/nvim-jdtls', -- java language server
+    'nvim-lua/plenary.nvim', -- additional lua functions
+    'neovim/nvim-lspconfig', -- language server configuration
+    'rafaqz/ranger.vim',  -- use ranger as file browser
+    'ellisonleao/gruvbox.nvim', -- colorscheme
+    'jlanzarotta/bufexplorer', -- better buffer navigation
+    'echasnovski/mini.map', -- mini map (buffer text overview)
+    'tpope/vim-fugitive', -- git integration
+    'scrooloose/nerdcommenter', -- code commenting
+    'nielsadb/vim-filtering', -- filter lines in buffer
+    {
+        'nvim-telescope/telescope.nvim', tag = '0.1.5',
+        dependencies = {
+            'nvim-lua/plenary.nvim',
+            'ryanoasis/vim-devicons',
+        },
     },
-  },
-  'AndrewRadev/sideways.vim', -- move function args, etc. around
-  {
-     'vim-airline/vim-airline',
-     dependencies = {
-      'nvim-tree/nvim-web-devicons',
-      'ryanoasis/vim-devicons',
-      'nvim-treesitter/nvim-treesitter',
-      'sharkdp/fd',
-     },
-  }, -- powerline-like status bar
-  'vim-airline/vim-airline-themes', -- themes for the above
-  'nvim-tree/nvim-web-devicons', -- additional icons
-  'ryanoasis/vim-devicons', -- additional icons
+    'AndrewRadev/sideways.vim', -- move function args, etc. around
+    {
+        'vim-airline/vim-airline',
+        dependencies = {
+            'nvim-tree/nvim-web-devicons',
+            'ryanoasis/vim-devicons',
+            'nvim-treesitter/nvim-treesitter',
+            'sharkdp/fd',
+        },
+    },
 
-  {'nvim-treesitter/nvim-treesitter', build = ':TSUpdate'}, -- tree-sitter AST integration (requried for aerial below)
-  'stevearc/aerial.nvim', -- code outliner (tagbar replacement)
+    -- powerline-like status bar
+    'vim-airline/vim-airline-themes', -- themes for the above
+    'nvim-tree/nvim-web-devicons', -- additional icons
+    'ryanoasis/vim-devicons', -- additional icons
 
-  -- syntax highlighting
-  'trapd00r/irc.vim', -- IRC logs
-  'Glench/Vim-Jinja2-Syntax', -- jinja2 templates
-  'vim-scripts/django.vim', -- Django templates
-  'tsandall/vim-rego', -- rego (OPA policy language)
-  'aklt/plantuml-syntax', -- PlantUML
-  'nathanalderson/yang.vim', -- YANG
-  'nblock/vim-dokuwiki', -- DokuWiki
+    {'nvim-treesitter/nvim-treesitter', build = ':TSUpdate'}, -- tree-sitter AST integration (requried for aerial below)
+    'stevearc/aerial.nvim', -- code outliner (tagbar replacement)
+
+    -- autocompletion
+    'neovim/nvim-lspconfig',
+    'hrsh7th/cmp-nvim-lsp',
+    'hrsh7th/cmp-buffer',
+    'hrsh7th/cmp-path',
+    'hrsh7th/cmp-cmdline',
+    'hrsh7th/nvim-cmp',
+    'hrsh7th/cmp-vsnip',
+    'hrsh7th/vim-vsnip',
+
+    -- syntax highlighting
+    'trapd00r/irc.vim', -- IRC logs
+    'Glench/Vim-Jinja2-Syntax', -- jinja2 templates
+    'vim-scripts/django.vim', -- Django templates
+    'tsandall/vim-rego', -- rego (OPA policy language)
+    'aklt/plantuml-syntax', -- PlantUML
+    'nathanalderson/yang.vim', -- YANG
+    'nblock/vim-dokuwiki', -- DokuWiki
 })
 
 -- vim: set et sts=4 sw=4:
